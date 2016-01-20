0ac8fe6025b05f7348a12f4b7718d096084ff81e
Willem Strauss <willems@imqs.local>
2016-01-20 11:56:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Golang_sandbox
+Random Go code I'm playing with.
+
+Sorry that you have to see this, but a private repo cost money :)
---
+++ src/github.com/wastrauss/hello/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/wastrauss/stringutil"
+)
+
+func main() {
+	fmt.Printf(stringutil.Reverse("melliW olleH"))
+}
